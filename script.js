6b20bffd98b7d6cb5a5b7b01807f372fce49a753
kartikeypatel1 <kp0543985@gmail.com>
2024-12-12 15:31:06 +0530

    this is added on 12/12/24

---
+++ script.js
@@ -0,0 +1,54 @@
+// function sayHello(){alert("HEllo World!")}
+// // document.write('<h1>this is my first javascript page</h1>'
+// // )
+// // var pi=3.14;
+// // var persion="are you GOD";
+// // var answer='yes i am!';
+// // document.write(pi="<br>");
+// // document.write(person+"<br>");
+// // document.write(answer+"<br>");
+// function myfunction(){
+//     var carnname="welcome";
+//     document.get
+// }
+// var person = {
+//     firstname: "John",
+//     lastName: "Doe",
+//     id: 5566,
+//     getinfo: function() {
+//         return this.firstname + " " + this.lastName;
+//     }
+// };
+// document.getElementById("demo").innerHTML = person.getinfo();
+
+// var person={
+//     name:'ramesh',
+//     age:30,
+//     isMarried:true,
+//     address:{
+//         street:'vinay nagar',
+//         flatno:201
+//     }
+// };
+// console.log(person);
+
+
+
+
+// var person={
+//     name:'kartikey patel',
+//     age:20,
+//     ismarried:true,
+//     child:['cheeku'],
+//     getInfo:function(){
+//     },
+//     address:{
+//         street:'vinay nagar',
+//         flatno:201
+//     }
+// };
+// console.log(person['name'])
+// document.write(person['name'])
+
+
+
